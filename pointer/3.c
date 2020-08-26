d6c1ba5f91a6f89938c86fac8c1d77997d442bf4
wmf1997 <wmf997@126.com>
2020-08-26 14:18:56 +0800

    try to use VSCode to manage this clion project.

---
+++ pointer/3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (int argc, const char* argv[])
+{
+    float fa[3] = {4.13, 5.4, 6.66};
+    float *pfa = fa;
+    printf("%.4f\n", *(pfa));
+    for (int i = 1; i < 3; i++)
+    {
+        printf("%.4f\n", *(pfa+i));
+    }
+    return 0;  // 之前写的是0x0, 是因为公主连结里面的可可萝的表情包, 之后还是回来吧. 
+}
