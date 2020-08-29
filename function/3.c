46ce709b059b0c9c0eda90dc41fc346d09fa10fc
wmf997 <wmf997@126.com>
2020-08-29 14:39:22 +0800

    好好干活吧

--- function/3.c
+++ function/3.c
@@ -26,14 +26,21 @@ float* max_1 (float* input_arr, int N)
 
 int main (int argc, const char** argv[])
 {
-    float a[5] = { 0, 222, 99, 1, 3 };
-    float* ptr_max = max_1(a, 5);
+    float a[5] = { 0, 299, 99, 1111, 333 };
     float my_max = max_0(a, 5);
-    float* (*ptr_func_max)(float* fuck_input_ptr, int NMSL);
-    ptr_func_max = max_1;
+    float* ptr_max; 
+    ptr_max = max_1(a, 5);
+    
     printf("%f\n", *ptr_max);
     printf("%f\n", my_max);
-    float* ptr_max_1 = (*ptr_func_max)(a, 5);
+    
+    float (*ptr_func_max_0)(float* an_arr, int M) = max_0;
+    float* (*ptr_func_max_1)(float* fuck_input_ptr, int NMSL);
+    ptr_func_max_1 = max_1;
+    
+    float ptr_max_0 = (*ptr_func_max_0)(a, 5);
+    float* ptr_max_1 = (*ptr_func_max_1)(a, 5);
+    printf("%f\n", ptr_max_0);
     printf("%f\n", *ptr_max_1);
     return 0;
 }
