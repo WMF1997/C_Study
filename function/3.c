caeea15a6c3d5d2b3d520b048e798bfa0809da27
wmf997 <wmf997@126.com>
2020-08-29 14:27:55 +0800

    出于各种原因, 就算是CMakeLists.txt也不再提供了, 就是这么任性. 我们的重点是关注代码本身的实现, 如果说将我的代码配置到各种工程上的话, 我会在wiki上写的~

--- function/3.c
+++ function/3.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 // this is an example
+float max_0(float* input_arr, int N)
+{
+    float current_max = *input_arr;
+    for (int i=0; i<N; i++)
+        current_max = (*(input_arr + i) > current_max) ? (*(input_arr + i)) : (current_max);  // 采用保守治疗的方法. 
+    return current_max;
+}
+
 float* max_1 (float* input_arr, int N)
 {
     int rel = 0;
@@ -20,6 +28,12 @@ int main (int argc, const char** argv[])
 {
     float a[5] = { 0, 222, 99, 1, 3 };
     float* ptr_max = max_1(a, 5);
+    float my_max = max_0(a, 5);
+    float* (*ptr_func_max)(float* fuck_input_ptr, int NMSL);
+    ptr_func_max = max_1;
     printf("%f\n", *ptr_max);
+    printf("%f\n", my_max);
+    float* ptr_max_1 = (*ptr_func_max)(a, 5);
+    printf("%f\n", *ptr_max_1);
     return 0;
 }
